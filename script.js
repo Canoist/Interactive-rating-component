85c39ea135e4c9b3d15bde1f4a741170694f9fb4
Leonid Lichkun <lionproklon@gmail.com>
2022-05-09 03:47:53 +0300

    Final result

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-const li = document.querySelectorAll("li");
-const btn = document.querySelector("button");
+const ul = document.querySelector("ul");
+const form = document.querySelector("form");
+const result = document.querySelector(".result");
+const finalCard = document.querySelector(".center");
 
-li
+ul.addEventListener("click", (event) => {
+  const liAll = document.querySelectorAll("li");
+  liAll.forEach((li) => li.classList.remove("selected"));
+  result.textContent = `You selected out ${event.target.innerText} of 5`;
+  event.target.classList.add("selected");
+});
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  form.classList.toggle("hidden");
+  finalCard.classList.toggle("hidden");
+});
